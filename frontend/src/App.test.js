ecc79cf0c776547e102749b2728f37403e53669b
shiraz09 <shiraz120010@gmail.com>
2025-01-21 18:51:47 +0200

    .

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the app without crashing', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Products/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
